7884ee96640412945fb9a0a5f61399d222e53881
SyaifullohWD <66584238+SyaifullohWD@users.noreply.github.com>
2020-12-07 20:31:35 +0700

    +logout

--- Luqis/app/src/main/java/com/example/luqis/MainActivity.java
+++ Luqis/app/src/main/java/com/example/luqis/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.luqis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLogin, btnLogout;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        if (fAuth.getCurrentUser() != null){
+        btnLogout.setVisibility(View.VISIBLE);
+        btnLogin.setVisibility(View.INVISIBLE);
+        }else {
+            btnLogout.setVisibility(View.INVISIBLE);
+            btnLogin.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
+    }
+
+    public void login(View view) {
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
     }
 }
