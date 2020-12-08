47bd7fa74b7d292cf3d7e925192eb7122884faea
arfiandi <alibiabu16@gmail.com>
2020-12-08 16:59:52 +0700

    reBuild APP

---
+++ Luqis_/app/src/main/java/com/example/luqis_/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.luqis_.Prevalent;
+
+import com.example.luqis_.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
